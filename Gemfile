322927b0d0474290471a8055bad3a757c658ca87
Yilen <yilen.pan@gmail.com>
2015-02-02 23:37:54 -0800

    adds figaro, dropbox, and awesomeness

--- Gemfile
+++ Gemfile
@@ -1,8 +1,9 @@
 source 'https://rubygems.org'
-ruby "2.0.0"
+ruby "2.1.1"
 
 gem 'rails', '4.2.0'
 gem 'sass-rails', '~> 5.0'
+gem "figaro"
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
@@ -11,6 +12,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'bootstrap-sass', '~> 3.3.3'
 gem "paperclip", "~> 4.2"
+gem "paperclip-dropbox", ">= 1.1.7"
 
 group :production do
 	gem 'pg'
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,8 @@
 class Listing < ActiveRecord::Base
-	has_attached_file :image, :styles => { :medium => "200x200", :thumb => "100x100>" }, :default_url => "omg.jpg"
+	has_attached_file :image, :styles => { :medium => "200x200", :thumb => "100x100>" }, :default_url => "omg.jpg",   
+					  :storage => :dropbox,
+   					  :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+   					  #:dropbox_options => {...}
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
 end
