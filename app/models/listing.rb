8ebc5e85ce2769a264b5937d23176cd26063a02a
Yilen <yilen.pan@gmail.com>
2015-02-02 23:55:02 -0800

    added clause stating dropbox for deployment, local for localhost

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,8 +1,13 @@
 class Listing < ActiveRecord::Base
-	has_attached_file :image, :styles => { :medium => "200x200", :thumb => "100x100>" }, :default_url => "omg.jpg",   
-					  :storage => :dropbox,
-   					  :dropbox_credentials => Rails.root.join("config/dropbox.yml")
-   					  #:dropbox_options => {...}
+	if Rails.env.development?
+		has_attached_file :image, :styles => { :medium => "200x200", :thumb => "100x100>" }, :default_url => "omg.jpg"
+	else
+		has_attached_file :image, :styles => { :medium => "200x200", :thumb => "100x100>" }, :default_url => "omg.jpg",   
+						  :storage => :dropbox,
+						  :path => ":style/:id_:filename",
+	   					  :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+	   					  #:dropbox_options => {...}
+	end
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
 end
