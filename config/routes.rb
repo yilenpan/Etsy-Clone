9d738f92a55d163d220bcd0cccffdf4a9681025a
Yilen <yilen.pan@gmail.com>
2015-02-25 22:24:50 -0800

    Add seller page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'pages/contact'
   #get 'listings#listings'
   root 'listings#index'
+  get 'seller' => "listings#seller"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
