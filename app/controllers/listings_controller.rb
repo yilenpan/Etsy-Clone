7d1031be0ae53c5b922d6d6c535f0038f7176727
Yilen <yilen.pan@gmail.com>
2015-02-26 02:26:05 -0800

    more bugs

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -44,9 +44,11 @@ class ListingsController < ApplicationController
         :type => "individual",
         :bank_account => token
         )
+      current_user.recipient = recipient.id
+      current_user.save
     end
-    current_user.recipient = recipient.id
-    current_user.save
+
+
 
     respond_to do |format|
       if @listing.save
